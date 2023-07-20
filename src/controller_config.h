a4da6b22af8110c24bd73c9bf38132206e24c2e3
Ian <14359681+kuwoh@users.noreply.github.com>
2023-07-20 10:35:37 +0800

    Update controller_config.h
    added more than 16 btns
    note: find out how to get rid of unused like - leds, enc, etc.

--- src/controller_config.h
+++ src/controller_config.h
@@ -20,10 +20,10 @@ const uint8_t SW_KEYCODE[] = {HID_KEY_D, HID_KEY_F, HID_KEY_J, HID_KEY_K,
                               HID_KEY_C, HID_KEY_M, HID_KEY_A, HID_KEY_B,
                               HID_KEY_1, HID_KEY_E, HID_KEY_G};
 const uint8_t SW_GPIO[] = {
-    4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 27,
+    4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22
 };
 const uint8_t LED_GPIO[] = {
-    5, 7, 9, 11, 13, 15, 17, 19, 21, 26,
+    26, 27
 };
 const uint8_t ENC_GPIO[] = {0, 2};      // L_ENC(0, 1); R_ENC(2, 3)
 const bool ENC_REV[] = {false, false};  // Reverse Encoders

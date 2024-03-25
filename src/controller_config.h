f34526ed4f3d286db46aae34685e6ef0bcb4d849
Ian <14359681+kuwoh@users.noreply.github.com>
2024-03-25 08:57:44 +0800

    Update controller_config.h

--- src/controller_config.h
+++ src/controller_config.h
@@ -1,8 +1,8 @@
 #ifndef CONTROLLER_CONFIG_H
 #define CONTROLLER_CONFIG_H
 
-#define SW_GPIO_SIZE 18               // Number of switches
-#define LED_GPIO_SIZE 3              // Number of switch LEDs
+#define SW_GPIO_SIZE 16               // Number of switches
+#define LED_GPIO_SIZE 0              // Number of switch LEDs
 #define ENC_GPIO_SIZE 2               // Number of encoders
 #define ENC_PPR 600                   // Encoder PPR
 #define MOUSE_SENS 1                  // Mouse sensitivity multiplier
@@ -21,13 +21,12 @@
 const uint8_t SW_KEYCODE[] = {HID_KEY_4, HID_KEY_5, HID_KEY_6, HID_KEY_7,
                               HID_KEY_R, HID_KEY_T, HID_KEY_Y, HID_KEY_U,
                               HID_KEY_F, HID_KEY_G, HID_KEY_H, HID_KEY_J,
-                              HID_KEY_V, HID_KEY_B, HID_KEY_N, HID_KEY_M,
-                              HID_KEY_I, HID_KEY_O};
+                              HID_KEY_V, HID_KEY_B, HID_KEY_N, HID_KEY_M};
 const uint8_t SW_GPIO[] = {
-    4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
 };
 const uint8_t LED_GPIO[] = {
-    22, 26, 27,
+    
 };
 const uint8_t ENC_GPIO[] = {0, 2};      // L_ENC(0, 1); R_ENC(2, 3)
 const bool ENC_REV[] = {false, false};  // Reverse Encoders
